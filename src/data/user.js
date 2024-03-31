f90c2116505cafceef9726862c809dd39862ca6e
Eddie-OConnor <eddie.oconnor3@gmail.com>
2024-03-31 13:13:39 -0400

    initial build

---
+++ src/data/user.js
@@ -0,0 +1,79 @@
+const INFO = {
+	main: {
+		title: "Ed the dev",
+		name: "Eddie O'Connor",
+		email: "eddie.oconnor3@gmail.com",
+		logo: "../headshot.png",
+	},
+
+	socials: {
+		twitter: "https://twitter.com/",
+		github: "https://github.com/Eddie-OConnor",
+		linkedin: "https://www.linkedin.com/in/edwardjamesoconnor/",
+		instagram: "https://instagram.com/",
+		stackoverflow: "https://stackoverflow.com/",
+		facebook: "https://facebook.com/",
+	},
+
+	homepage: {
+		title: "Passionate software engineer specializing in frontend work. I love building apps to solve real world challenges that are a delight to use for the end user.",
+		description:
+			"A coding curiosity (thanks Bitcoin) in late 2022 developed into a passion. My specialty at the moment is <strong>frontend development</strong>; however, I am actively researching backend technologies to build additional personal projects and add to my portfolio. These projects include, but are not limited to, tools for <strong>non-KYC verification</strong> and <strong>distinguishing AI generated content from non-AI.</strong>",
+	},
+
+	about: {
+		title: "Get to know me!",
+		description:
+			`My professional background is in ad tech. Most recently I was a solution engineer and collaborated with our sales team during the DSP pre sale cycle. This included giving SaaS product demos and coordinating the completion of RFIs. Additionally, I would provide platform improvement ideas to our product team based on client feedback and my own experience while being a hands on key user. When I'm not coding, I am an avid endurance athlete, home chef, bookworm, and DIYer who is as avid about 
+            those things as one can be while being a new dad.`,
+	},
+
+	projects: [
+		{
+			title: "Project 1",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 2",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 3",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 4",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 5",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+	],
+};
+
+export default INFO;
