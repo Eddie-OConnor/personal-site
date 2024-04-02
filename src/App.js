a474269f516f94598d57abc54752218dd73b7a80
Eddie-OConnor <eddie.oconnor3@gmail.com>
2024-04-02 16:49:57 -0400

    added scrollToTop component when navigating between pages

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import { Routes, Route } from "react-router-dom";
 import Homepage from "./pages/homepage";
 import About from "./pages/about";
 import Projects from "./pages/projects";
-
 import Contact from "./pages/contact";
 import Notfound from "./pages/404";
+import ScrollToTop from "./components/common/ScrollToTop";
 
 import "./app.css";
 
@@ -17,10 +17,10 @@ function App() {
 				<Route path="/" element={<Homepage />} />
 				<Route path="/about" element={<About />} />
 				<Route path="/projects" element={<Projects />} />
-
 				<Route path="/contact" element={<Contact />} />
 				<Route path="*" element={<Notfound />} />
 			</Routes>
+            <ScrollToTop />
 		</div>
 	);
 }
