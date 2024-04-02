5f89be443fb76c249d1be4709646a6b0f3c2537f
Eddie-OConnor <eddie.oconnor3@gmail.com>
2024-04-02 14:26:28 -0400

    removing GA tracking until I learn more and can speak to it intelligently

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
-import ReactGA from "react-ga4";
 
 import Homepage from "./pages/homepage";
 import About from "./pages/about";
@@ -9,15 +7,9 @@ import Projects from "./pages/projects";
 import Contact from "./pages/contact";
 import Notfound from "./pages/404";
 
-import { TRACKING_ID } from "./data/tracking";
 import "./app.css";
 
 function App() {
-	useEffect(() => {
-		if (TRACKING_ID !== "") {
-			ReactGA.initialize(TRACKING_ID);
-		}
-	}, []);
 
 	return (
 		<div className="App">
