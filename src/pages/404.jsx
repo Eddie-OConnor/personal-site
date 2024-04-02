c3147116c4fad180e5123cae409287e752f61ede
Eddie-OConnor <eddie.oconnor3@gmail.com>
2024-04-02 12:54:03 -0400

    logo component removal

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -4,7 +4,6 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFaceSadTear } from "@fortawesome/free-regular-svg-icons";
 
 import NavBar from "../components/common/navBar";
-import Logo from "../components/common/logo";
 
 import INFO from "../data/user";
 
@@ -20,11 +19,6 @@ const Notfound = () => {
 			<div className="not-found page-content">
 				<NavBar />
 				<div className="content-wrapper">
-					<div className="notfound-logo-container">
-						<div className="projects-logo">
-							<Logo width={46} />
-						</div>
-					</div>
 					<div className="notfound-container">
 						<div className="notfound-message">
 							<div className="notfound-title">
